b51b501bd9e4abd404ba7c6a754ee4f61eea47bb
Kareem Diaa Helal <kareemdiaahelal@gmail.com>
2025-03-20 06:35:09 +0200

    exact code

--- Caesar Cipher Code.py
+++ Caesar Cipher Code.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def caesar_cipher(text, shift, mode='encrypt'):
     result = ""
     
@@ -23,29 +22,3 @@ mode = input("Type 'encrypt' or 'decrypt': ").lower()
 
 output = caesar_cipher(message, shift, mode)
 print(f"Result: {output}")
-=======
-def caesar_cipher(text, shift, mode='encrypt'):
-    result = ""
-    
-    if mode == 'decrypt':
-        shift = -shift
-    
-    for char in text:
-        if char.isalpha():
-            shift_base = ord('A') if char.isupper() else ord('a')
-            result += chr((ord(char) - shift_base + shift) % 26 + shift_base)
-        else:
-            result += char
-    
-    return result
-# qq
-# Main
-print("Welcome to Caesar Cipher Program!")
-print("=================================")
-message = input("Enter your message: ")
-shift = int(input("Enter shift value: "))
-mode = input("Type 'encrypt' or 'decrypt': ").lower()
-
-output = caesar_cipher(message, shift, mode)
-print(f"Result: {output}")
->>>>>>> 500337464ccd2ebf3722742a2be403961efe673f
